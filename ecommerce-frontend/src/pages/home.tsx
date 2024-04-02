cf5af797665a0133b60396ae12a8d7194c70788b
Soumyadeep Mallick <81796438+Soumya-2003@users.noreply.github.com>
2024-04-02 21:21:51 +0530

    product card added

--- ecommerce-frontend/src/pages/home.tsx
+++ ecommerce-frontend/src/pages/home.tsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom"
+import ProductCard from "../components/product-card"
 
 
 
 const Home = () => {
+
+  const addToCartHandler = () => {
+
+  }
+
   return (
     <div className="home">
       <section></section>
@@ -11,7 +17,9 @@ const Home = () => {
       <Link to={"/search"} className="findmore">More</Link>
     </h1>
 
-    <main></main>
+    <main>
+      <ProductCard productId="adasdasd" name="macbook" price={109100} stock={546} handler={addToCartHandler} photo="https://m.media-amazon.com/images/I/71ItMeqpN3L._AC_UY327_FMwebp_QL65_.jpg" />
+    </main>
 
     </div>
   )
