1c4993bcf72968a5e7791a4b9a75cce159824181
Soumyadeep Mallick <81796438+Soumya-2003@users.noreply.github.com>
2024-04-04 21:31:34 +0530

    Modifying cart-items

--- ecommerce-frontend/src/pages/cart.tsx
+++ ecommerce-frontend/src/pages/cart.tsx
@@ -1,7 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { VscError } from "react-icons/vsc";
+import CartItem from "../components/cart-item";
 
-const cartItems = [];
+const cartItems = [
+
+  {
+
+    productId:"bjhgjkj",
+    photo:"https://m.media-amazon.com/images/I/71ItMeqpN3L._AC_UY327_FMwebp_QL65_.jpg",
+    name:"MacBook",
+    price:3000,
+    quantity:4,
+    stock:10,
+
+  }
+
+];
 const subtotal = 4000;
 const tax = Math.round(subtotal*0.18);
 const shippingCharges = 200;
@@ -11,9 +25,33 @@ const total = subtotal + tax + shippingCharges;
 const Cart = () => {
   const [couponCode,setCouponCode] = useState<string>("");
   const [isValidCouponCode,setisValidCouponCode] = useState<boolean>(false);
+
+  useEffect(() => {
+
+    const timeOutID = setTimeout(() => {
+      if(Math.random()>0.5) setisValidCouponCode(true);
+      else setisValidCouponCode(false);
+    },1000);
+
+    return () => {
+      clearTimeout(timeOutID);
+      setisValidCouponCode(false);
+    }
+  }, [couponCode])
+
   return (
     <div className="cart">
-      <main></main>
+      <main>
+
+        {
+          cartItems.map((i,idx) => <CartItem key={idx}/>)
+        }
+
+
+
+
+
+      </main>
       <aside>
         <p>Subtotal: ₹{subtotal}</p>
         <p>Shipping Charges: ₹{shippingCharges}</p>
@@ -26,7 +64,7 @@ const Cart = () => {
         <input type="text" placeholder="Coupon Code" value={couponCode} onChange={(e) => setCouponCode(e.target.value)} />
 
         {
-          couponCode && isValidCouponCode? <span className="green">₹{discount} off using the <code>{couponCode}</code></span> : <span className="red">Invalid Coupon <VscError /></span>
+          couponCode && (isValidCouponCode? (<span className="green">₹{discount} off using the <code>{couponCode}</code></span>) : <span className="red">Invalid Coupon <VscError /></span>)
         }
 
       </aside>
