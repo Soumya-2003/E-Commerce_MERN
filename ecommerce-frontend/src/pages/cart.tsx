e39da83d0c199a99cdace553c010840eb12a7ebd
Soumyadeep Mallick <81796438+Soumya-2003@users.noreply.github.com>
2024-04-04 21:01:42 +0530

    Cart details is made

--- ecommerce-frontend/src/pages/cart.tsx
+++ ecommerce-frontend/src/pages/cart.tsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import { VscError } from "react-icons/vsc";
 
+const cartItems = [];
+const subtotal = 4000;
+const tax = Math.round(subtotal*0.18);
+const shippingCharges = 200;
+const discount = 400;
+const total = subtotal + tax + shippingCharges;
 
 const Cart = () => {
+  const [couponCode,setCouponCode] = useState<string>("");
+  const [isValidCouponCode,setisValidCouponCode] = useState<boolean>(false);
   return (
-    <div>
-      Cart
+    <div className="cart">
+      <main></main>
+      <aside>
+        <p>Subtotal: ₹{subtotal}</p>
+        <p>Shipping Charges: ₹{shippingCharges}</p>
+        <p>Tax: ₹{tax}</p>
+        <p>
+          Discount: <em> - ₹{discount}</em>
+        </p>
+        <p><b>Total: ₹{total}</b></p>
+
+        <input type="text" placeholder="Coupon Code" value={couponCode} onChange={(e) => setCouponCode(e.target.value)} />
+
+        {
+          couponCode && isValidCouponCode? <span className="green">₹{discount} off using the <code>{couponCode}</code></span> : <span className="red">Invalid Coupon <VscError /></span>
+        }
+
+      </aside>
     </div>
   )
 }
