d83a60f3bdd4caa98652592169f0df3c7a13423b
Soumyadeep Mallick <81796438+Soumya-2003@users.noreply.github.com>
2024-05-03 20:43:17 +0530

    User Schema done

--- ecommerce-backend/src/app.ts
+++ ecommerce-backend/src/app.ts
@@ -1,7 +1,24 @@
 import express from 'express';
+
+//   Importing Routes
+import userRoute from "./routes/user.js";
+
+
+
 const port = 4000;
 
 const app = express();
+
+//   Using Routes
+app.use("/api/v1/user", userRoute);
+
+// app.get("/users", (req, res) => {
+//     res.send("asdfds");
+// });
+
+// app.get("/product", (req, res) => {
+//     res.send("asdfds");
+// });
 
 app.listen(port,()=>{
     console.log(`Server os working on http://localhost:${port}`);
